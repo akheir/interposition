331af74823b202a40215aad50dac83ccfb325f08
Alireza Kheirkhahan <kheirkhahan@gmail.com>
2017-07-07 16:31:29 -0500

    adding posix interface support

--- interposition.cpp
+++ interposition.cpp
@@ -1,3 +1,8 @@
+//  Copyright (c) 2017 Alireza Kheirkhahang
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -33,15 +38,41 @@ extern "C" {
  		if (!(__real_ ## func)) \
  		std::cout  << "Failed to link symbol: " << #func << std::endl; \
 	}
+#if __GNUC__ >= 4
+    #define DLL_PUBLIC __attribute__ ((visibility ("default")))
+    #define DLL_LOCAL  __attribute__ ((visibility ("hidden")))
+#else
+    #define DLL_PUBLIC
+    #define DLL_LOCAL
+#endif
 
+/*
+ * File manipulation functions
+ *
+ * open, close, creat, read, write, close, pread, pwrite
+ *
+*/
+FORWARD_DECLARE(int, creat, (const char *path, mode_t mode));
 FORWARD_DECLARE(int, open, (const char *path, int flags, ...));
+FORWARD_DECLARE(int, close, (int fd));
+FORWARD_DECLARE(int, unlink, (const char *pathname));
+FORWARD_DECLARE(ssize_t, write, (int fd, const void *buf, size_t count));
+FORWARD_DECLARE(ssize_t, read, (int fd, void *buf, size_t count));
+FORWARD_DECLARE(ssize_t, pread, (int fd, void *buf, size_t count, off_t offset));
+FORWARD_DECLARE(ssize_t, pwrite, (int fd, const void *buf, size_t count, off_t offset));
+
 FORWARD_DECLARE(FILE *, fopen, (const char *path, const char *mode));
 
-int open(const char *path, int flags, ...) {
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+DLL_PUBLIC int open(const char *path, int flags, ...) {
 	MAP(open,int (*)(const char*, int, ...));
 
-	printf("my open\n");
-	int ret;
+    std::cout << "Intercepting a call to open \"" << path << '\"' << std::endl;
+
+    int ret = 0;
 
 	if ((flags & O_CREAT) == O_CREAT) {
 		va_list argf;
@@ -53,15 +84,101 @@ int open(const char *path, int flags, ...) {
 		ret = __real_open(path, flags);
 	}
 
-	std::cout << "opening file: " << std::endl;
+    std::cout << path << " opened successfully with fd: \"" << ret << '\"' << std::endl;
 
 	return ret;
 }
 
-FILE * fopen(const char *path, const char *mode) {
+DLL_PUBLIC int creat(const char* path, mode_t mode) {
+
+    std::cout << "Intercepting a call to creat \"" << path << '\"' << std::endl;
+
+    int ret = open(path, O_WRONLY | O_CREAT | O_TRUNC, mode);
+
+    std::cout << path << " created successfully with fd: \"" << ret << '\"' << std::endl;
+
+    return ret;
+}
+
+DLL_PUBLIC int close(int fd) {
+    MAP(close,int (*)(int));
+
+    std::cout << "Intercepting a call to close \"" << fd << '\"' << std::endl;
+
+    int ret = __real_close(fd);
+
+    return ret;
+}
+
+DLL_PUBLIC int unlink(const char *pathname) {
+    MAP(unlink, int (*)(const char *));
+
+    std::cout << "Intercepting a call to unlink \"" << pathname << '\"' << std::endl;
+
+    int ret = 0;
+    ret = __real_unlink(pathname);
+
+    return ret;
+}
+
+DLL_PUBLIC ssize_t write(int fd, const void *buf, size_t count) {
+    MAP(write,ssize_t (*)(int, const void*, size_t));
+
+    std::cout << "Intercepting a call to write "
+              << count << " bytes on \"" << fd << '\"' << std::endl;
+
+    ssize_t ret = 0;
+    ret = __real_write(fd, buf, count);
+
+    return ret;
+}
+
+DLL_PUBLIC ssize_t read(int fd, void *buf, size_t count) {
+    MAP(read, ssize_t(*)(int, void * , size_t));
+
+    std::cout << "Intercepting a call to read "
+              << count << " bytes from \"" << fd << '\"' << std::endl;
+
+    ssize_t ret = 0;
+    ret = __real_read(fd, buf, count);
+
+    return ret;
+}
+
+DLL_PUBLIC ssize_t pread(int fd, void *buf, size_t count, off_t offset) {
+    MAP(pread,ssize_t (*)(int, void*, size_t, off_t));
+
+    std::cout << "Intercepting a call to pread "
+              << count << " bytes from \"" << fd << '\"' <<
+              " on position " << offset << std::endl;
+
+    ssize_t ret = 0;
+    ret = __real_pread(fd, buf, count, offset);
+
+    return ret;
+}
+
+DLL_PUBLIC ssize_t pwrite(int fd, const void *buf, size_t count, off_t offset) {
+    MAP(pwrite,ssize_t (*)(int, const void*, size_t, off_t));
+
+    std::cout << "Intercepting a call to pwrite "
+              << count << " bytes on \"" << fd << '\"' <<
+              " on position " << offset << std::endl;
+
+    ssize_t ret = 0;
+    ret = __real_pwrite(fd, buf, count, offset);
+
+    return ret;
+}
+
+DLL_PUBLIC FILE * fopen(const char *path, const char *mode) {
 	MAP(fopen, FILE *(*)(const char *, const char *));
 
 	std::cout << "fopen: " << path << std::endl;
 
 	return __real_fopen(path, mode);
 }
+
+#ifdef __cplusplus
+} //extern "C" {
+#endif
